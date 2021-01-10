2d64a0e4c6358a537f8f30a0b2c1aca128eaf39a
shoneriki <ikirenohs@gmail.com>
2021-01-11 01:00:44 +0900

    z-index error corrects header glitch

--- source/assets/javascripts/main.js
+++ source/assets/javascripts/main.js
@@ -8,13 +8,7 @@
 function toggle() {
   const toggler = document.querySelector('.menu-toggler');
   const topNav = document.querySelector('.top-nav');
-  const landing = document.querySelector('.landing-text');
   toggler.classList.toggle("open");
   topNav.classList.toggle("open");
-  if (topNav.classList.contains('open')) {
-    landing.style.display = 'none';
-  } else {
-    landing.style.display = 'block';
-  }
 }
 // toggle()
