b8e4e2beacaf4666d8e8ed7ec56b69c9c8f921a8
shoneriki <ikirenohs@gmail.com>
2021-01-10 23:42:43 +0900

    menu opens JS

---
+++ source/assets/javascripts/main.js
@@ -0,0 +1,13 @@
+// $(document).ready(function () {
+//   $('.menu-toggler').on('click', function() {
+//     $(this).toggleClass('open');
+//     $('.top-nav').toggleClass('open');
+//   })
+// })
+
+function toggle() {
+  const toggler = document.querySelector('.menu-toggler');
+  const topNav = document.querySelector('.top-nav');
+  toggler.classList.toggle("open");
+  topNav.classList.toggle("open");
+}
