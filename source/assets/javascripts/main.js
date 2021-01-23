83a7bc6ec51bf6796c981ed49e7816503c564626
shoneriki <ikirenohs@gmail.com>
2021-01-24 00:31:41 +0900

    responsive hamburger menu start

--- source/assets/javascripts/main.js
+++ source/assets/javascripts/main.js
@@ -1,6 +1,19 @@
 //= require jquery;
 window.addEventListener('DOMContentLoaded', function() {
-  // get the form elements defined in your form HTML above
+  const mainMenu = document.querySelector('.main-menu');
+  const closeMenu = document.querySelector('.close-menu');
+  const openMenu = document.querySelector('.open-menu');
+
+  openMenu.addEventListener('click', show);
+  closeMenu.addEventListener('click', close);
+
+  function show(){
+      mainMenu.style.display = 'flex';
+      mainMenu.style.top = '0';
+  }
+  function close(){
+      mainMenu.style.top = '-100%';
+  }
 
   var form = document.getElementById("my-form");
   // var button = document.getElementById("my-form-button");
