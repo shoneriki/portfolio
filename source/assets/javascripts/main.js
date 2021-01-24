a6cdb8903746303efc2ecc8d06e4cbd11a863d83
shoneriki <ikirenohs@gmail.com>
2021-01-25 00:04:09 +0900

    functional navbar with font-change

--- source/assets/javascripts/main.js
+++ source/assets/javascripts/main.js
@@ -1,21 +1,9 @@
 //= require jquery;
 window.addEventListener('DOMContentLoaded', () => {
-  const mainMenu = document.querySelector('.main-menu');
-  const closeMenu = document.querySelector('.close-menu');
-  const openMenu = document.querySelector('.open-menu');
+
   const form = document.getElementById('my-form');
   // var button = document.getElementById("my-form-button");
   const status = document.getElementById('status');
-
-  openMenu.addEventListener('click', show);
-  closeMenu.addEventListener('click', close);
-
-function show(){
-  mainMenu.style.display = 'flex';
-  mainMenu.style.top = '0';
-}
-function close(){
-  mainMenu.style.top = '-100%';
 }
 
   // Success and Error functions for after the form is submitted
