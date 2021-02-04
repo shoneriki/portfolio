c4727ae90c1ca0e2551be2668a4daa0d3a0c6687
shoneriki <ikirenohs@gmail.com>
2021-02-04 13:12:26 +0900

    form

--- source/assets/javascripts/main.js
+++ source/assets/javascripts/main.js
@@ -4,7 +4,7 @@ window.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('my-form');
   // var button = document.getElementById("my-form-button");
   const status = document.getElementById('status');
-  
+
 
   // Success and Error functions for after the form is submitted
 
@@ -42,71 +42,3 @@ function ajax(method, url, data, success, error) {
   };
   xhr.send(data);
 }
-
-
-
-// $(document).ready(function () {
-//   $('.menu-toggler').on('click',function () {
-//     $(this).toggleClass('open');
-//     $('.top-nav').toggleClass('open');
-//   });
-
-//   $('.top-nav .nav-link').on('click', function () {
-//     $('.menu-toggler').removeClass('open');
-//     $('.top-nav').removeClass('open');
-//   });
-
-//   // $('nav a[href*="#"]').on('click', function () {
-//   //   $('html, body').animate( keyframes: {
-//   //     scrollTop: $($(this).attr('href')).offset().top - 100
-//   //   }, options:2000);
-//   // });
-
-//   // $('#up').on('click', function () {
-//   //   $('html, body').animate(keyframes:{
-//   //     scrollTop: 0
-//   //   }, options: 2000);
-//   // });
-// });
-
-// const toggler = document.querySelector('.menu-toggler');
-// const openToggler = document.querySelector('.menu-toggler.open');
-// const topNav = document.querySelector('.top-nav');
-// const navList = document.querySelector('.nav-list');
-// const navLink = document.querySelectorAll('.nav-link');
-
-// function toggle() {
-//   if (openToggler) {
-//   toggler.addEventListener('click', () => {
-//     toggler.classList.toggle('open');
-//     topNav.classList.toggle('open');
-//     });
-//   }
-// }
-
-
-// function closeNav() {
-//   toggler.removeClass('open');
-//   navList.classList.removeClass('open');
-// }
-
-// navLink.addEventListener('click', closeNav());
-
-// function toggle() {
-//   const toggler = document.querySelector('.menu-toggler');
-//   const topNav = document.querySelector('.top-nav');
-
-//   toggler.classList.toggle('open');
-//   topNav.classList.toggle('open');
-// }
-// toggle();
-
-// function closeNav() {
-//   const toggler = document.querySelector('.menu-toggler');
-//   const navList = document.querySelector('.nav-list');
-
-
-//   toggler.removeClass('open');
-//   navList.classList.removeClass('open');
-// }
-// closeNav();
